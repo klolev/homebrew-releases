b50493a5a8b544123f364dd42739ca21a12e93e5
Chloé Léveillé <chloe.leveille@quebecormedia.com>
2024-10-29 16:53:20 -0400

    added pomette

---
+++ Formula/Pomette.rb
@@ -0,0 +1,15 @@
+class Pomette < Formula
+  desc "🍎 Discord Rich Presence for Apple Music on macOS"
+  homepage ""
+  url "https://github.com/klolev/pomette/releases/download/v0.0.1/Pomette-0.0.1-Loose.zip"
+  sha256 "f9a6e567f4d307df0248328d72abed980f862b09a4278dd2f427bb54fd83eaf4"
+  license ""
+
+  def install
+    bin.install "Pomette"
+  end
+
+  test do
+    system "false"
+  end
+end
